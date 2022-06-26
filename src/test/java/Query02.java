f1bb0d27f81736e544a5adf02bc6587c7f38de98
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-06-26 20:52:28 +0300

    2606

--- src/test/java/Query02.java
+++ src/test/java/Query02.java
@@ -26,6 +26,17 @@ public class Query02 {
 
         System.out.println("==========================================================================");
 
+        ResultSet veri2= st.executeQuery("select * from ogrenciler where sinif=9 and cinsiyet='K'");
+
+        while (veri2.next()){
+            System.out.printf("%-6d %-15.15s %-8s %-8s\n", veri2.getInt(1), veri2.getString(2), veri2.getString(3), veri2.getString(4));
+        }
+
+        con.close();
+        st.close();
+        veri.close();
+        veri2.close();
+
     }
 
 }
